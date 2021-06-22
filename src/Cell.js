fc8949822770160848254750f4983e9aa3c533ca
Mac Rowe <m.rowe42@gmail.com>
2021-06-22 17:29:51 -0400

    commit tree a2b20b559274a9f5eff66d19fc441f126cfa37a2

---
+++ src/Cell.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class Cell extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            color: props.value
+        }
+    }
+
+    onMouseClick = (event) => {
+        this.setState({color: '#333'})
+    }
+
+    render() {
+        return <div onClick={this.onMouseClick} className="cell" style={{backgroundColor: this.state.color}}></div>
+
+    }
+
+}
+
+
+
+export default Cell
